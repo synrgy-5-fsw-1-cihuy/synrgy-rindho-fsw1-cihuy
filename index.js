2144a7e09d1a7e786949ea3c2e933c8ad697421d
Rindho Ananta <rindho.ananta@ti.ukdw.ac.id>
2022-11-30 20:43:14 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Node Modules
 const express = require('express')
-const formidableMiddleware = require('express-formidable');
-const { PORT = 8000 } = process.env
+const formidableMiddleware = require('express-formidable')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+const { PORT = 8002 } = process.env
 
 // Custom Modules
 const cars = require('./routers/cars')
@@ -9,10 +11,43 @@ const products = require('./routers/products')
 
 // Init
 const app = express()
-app.use(formidableMiddleware());
+app.use(formidableMiddleware())
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Binar Cars Express API with Swagger",
+            version: "0.1.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "LogRocket",
+                url: "https://logrocket.com",
+                email: "info@email.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:8002/api/",
+            },
+        ],
+    },
+    apis: [
+        "./routers/cars.js",
+        "./routers/products.js"
+    ],
+};
+
+const specs = swaggerJsDoc(options);
 
 // Routing
 app.use('/api/cars', cars)
 app.use('/api/products', products)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(PORT)
--- routers/products.js
+++ routers/products.js
@@ -1,4 +1,51 @@
 'use strict';
+/** 
+ *  @swagger
+ *  components:
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              required:
+ *                  - name
+ *                  - description
+ *                  - price
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      description: The auto-generated id of the product.
+ *                  name:
+ *                      type: string
+ *                      description: The name of your product.
+ *                  description:
+ *                      type: string
+ *                      description: The description of your product.
+ *                  price:
+ *                      type: integer
+ *                      description: The price of your product.
+ *                  createdAt:
+ *                      type: integer
+ *                      format: timestamptz
+ *                      description: The date of the record creation.
+ *                  updatedAt:
+ *                      type: integer
+ *                      format: timestamptz
+ *                      description: The date of the record creation.
+ */
+/**
+ *  @swagger
+ *  /products:
+ *      get:
+ *          description: Returns all products from the system that the user has access to
+ *          responses:
+ *              '200':
+ *                  description: List of products
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#/components/schemas/Product'
+ */
 
 const express = require('express')
 const router = express.Router()

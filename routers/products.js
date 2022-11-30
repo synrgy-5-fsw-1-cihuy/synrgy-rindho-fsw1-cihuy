633e3ba969e41ab9654134996df89e53b542e470
Rindho Ananta <rindho.ananta@ti.ukdw.ac.id>
2022-11-30 21:52:31 +0700

    open api

--- routers/products.js
+++ routers/products.js
@@ -84,6 +84,12 @@ router.put('/:id', productsController.updateFull)
  *                                  name: Aqua
  *                                  description: Air pegunungan
  *                                  price: 3000
+ *                          bakwan:
+ *                              summary: Bakwan product.
+ *                              value:
+ *                                  name: Bakwan
+ *                                  description: panas enak
+ *                                  price: 1500
  *          responses:
  *              '200':
  *                  description: Send back updated product.
@@ -96,6 +102,25 @@ router.put('/:id', productsController.updateFull)
  */
 router.patch('/:id', productsController.updatePartial)
 
+/**
+ *  @swagger
+ *  /products/{id}:
+ *      get:
+ *          description: Delete one product from the system that the user has access to
+ *          tags: [Products]
+ *          parameters:
+ *              - name: id
+ *                in: query
+ *                description: Id of product to get.
+ *                required: true
+ *          responses:
+ *              '200':
+ *                  description: Product object
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Product'
+ */
 router.delete('/:id', productsController.delete)
 
 module.exports = router

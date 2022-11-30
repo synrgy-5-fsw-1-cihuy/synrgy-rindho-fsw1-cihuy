af0c0361301bd73e8d2a0751721ddbac7884e607
hasan-dev <hasan01nov@gmail.com>
2022-11-30 21:44:12 +0700

    add api docs

--- routers/products.js
+++ routers/products.js
@@ -1,5 +1,5 @@
 'use strict';
-/** 
+/**
  *  @swagger
  *  components:
  *      schemas:
@@ -47,21 +47,48 @@
  *                                  $ref: '#/components/schemas/Product'
  */
 
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const productsController = require('../controllers/products.controller')
+const productsController = require('../controllers/products.controller');
 
-router.get('/', productsController.getAll)
+router.get('/', productsController.getAll);
 
-router.get('/:id', productsController.getOne)
+router.get('/:id', productsController.getOne);
 
-router.post('/', productsController.insertOne)
+/**
+ *  @swagger
+ *  /products:
+ *      post:
+ *          description: Insert one product to the system
+ *          tags: [Products]
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *                      examples:
+ *                          shoes:
+ *                              summary: Shoes product.
+ *                              value:
+ *                                  name: Shoes
+ *                                  description: this is shoes
+ *                                  price: 5000
+ *          responses:
+ *              '200':
+ *                  description: Product object inserted
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Product'
+ */
+router.post('/', productsController.insertOne);
 
-router.put('/:id', productsController.updateFull)
+router.put('/:id', productsController.updateFull);
 
-router.patch('/:id', productsController.updatePartial)
+router.patch('/:id', productsController.updatePartial);
 
-router.delete('/:id', productsController.delete)
+router.delete('/:id', productsController.delete);
 
-module.exports = router
+module.exports = router;
